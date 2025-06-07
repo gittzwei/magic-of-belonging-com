6d7993f5580fbda1292fa8a14f5c8bb11e1c7f0d
gittz2 <wainainaian9@gmail.com>
2025-06-07 15:39:54 +0300

    ret

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,15 +3,16 @@ import '../styles/globals.css';
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../styles/theme';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </ThemeProvider>
     </SessionProvider>
   );
 }
-
-export default MyApp;
